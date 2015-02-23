2e0eb70e6da2827723447268a19894caa96406f6
Nakia <nakiaaresz@gmail.com>
2015-02-23 16:19:26 +0100

    2-es elso resz pipa

--- Src/MozgoJatekElem.cs
+++ Src/MozgoJatekElem.cs
@@ -20,6 +20,36 @@ namespace OE.Prog2.Jatek.Jatekter
 
         protected void AtHelyez(int ujx, int ujy)
         {
+            JatekElem[] maselemek = ter.MegadottHelyenLevok(ujx, ujy);
+            double meret = 0;
+
+            for (int i = 0; i < maselemek.Length; i++)
+            {
+                maselemek[i].Utkozes(this);
+                if (!Aktiv)
+                    break;
+                else
+                    maselemek = ter.MegadottHelyenLevok(ujx, ujy);
+            }
+
+            for (int i = 0; i < maselemek.Length; i++)
+            {
+                this.Utkozes(maselemek[i]);
+                if (!Aktiv)
+                    break;
+                else
+                    maselemek = ter.MegadottHelyenLevok(ujx, ujy);
+            }
+
+            for (int i = 0; i < maselemek.Length; i++)
+                meret += maselemek[i].Meret;
+
+            if ((this.Meret + meret) <= 1)
+            {
+                this.X = ujx;
+                this.Y = ujy;
+            }
+
 
         }
     }
