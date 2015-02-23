df7c818c7d9a1658877107d4c7482c69caf0ee41
Nakia <nakiaaresz@gmail.com>
2015-02-23 16:56:55 +0100

    fal, jatekos osztalyok

---
+++ Src/Fal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OE.Prog2.Jatek.Jatekter;
+
+namespace OE.Prog2.Jatek.Szabalyok
+{
+    class Fal : RogzitettJatekElem
+    {
+        //az őséhez hasonlóan x és y és játéktér paramétereket kap, ezeket továbbítja az ősnek
+        public Fal(int x, int y, ref JatekTer ter)
+            : base(x, y, ref ter)
+        { }
+
+        //Mindig adjon vissza 1-et (tehát egy fal mellé más már biztos nem fog elférni
+        public override double Meret
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        //ne csináljon semmit, a fal passzív
+        public override void Utkozes(JatekElem jatekelem)
+        {
+        }
+    }
+}
---
+++ Src/Jatekos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using OE.Prog2.Jatek.Jatekter;
+
+namespace OE.Prog2.Jatek.Szabalyok
+{
+    class Jatekos : MozgoJatekElem
+    {
+        private string nev; //tárolja a játékos nevét
+
+        public string Nev
+        {
+            get { return nev; }
+        }
+
+        private int eletero = 100; //kezdőértéke 100. Tárolja a játékos életerejét
+        private int pontszam = 0; //kezdőértéke 0. Tárolja, hogy a játékos eddig hány pontot szerzett a játékban
+
+        //négy paramétere van: név, x, y, játéktér. Az elsőt eltárolja a megfelelő mezőben, a többit továbbítja az ős konstruktorának
+        public Jatekos(string nev, int x, int y, ref JatekTer ter)
+            : base(x, y, ref ter)
+        {
+            this.nev = nev;
+        }
+
+        //adjon vissza 0.2-t (tehát 5 játékos fér el egy mezőn, ha nincs ott más
+        public override double Meret
+        {
+            get
+            {
+                return 0.2;
+            }
+        }
+
+        //ne csináljon semmit
+        public override void Utkozes(JatekElem jatekelem)
+        {
+        }
+
+        public void Serul(int sebzes)
+        {
+            //Ha a játékos életereje már eleve 0, akkor ne csináljon semmit
+            if (eletero == 0) return;
+
+            //Ha nagyobb, akkor csökkentse az eletero-t a paraméterként átadott értékkel (de 0-nál kisebb ne legyen)
+            eletero -= sebzes;
+            if (eletero < 0)
+                eletero = 0;
+
+            //Ha a játékos életereje elérte a 0-t, akkor az Aktiv tulajdonságot állítsa hamisra
+            if (eletero == 0)
+                Aktiv = false;
+        }
+
+        //A megadott paraméterrel növeli a pontszam mező értékét.
+        public void PontotSzerez(int pluszpont)
+        {
+            pontszam += pluszpont;
+        }
+
+        //két paramétere két relatív koordináta (rx és ry, tipikusan -1, 0, 1 értékeket várunk ezekben).
+        //Számolja ki, hogy az aktuális pozícióból ezzel az elmozdulással hova jutna,
+        //és hívja meg az ősétől örökölt Athelyez függvényt ezekkel a koordinátákkal.
+        public void Megy(int rx, int ry)
+        {
+            this.AtHelyez(this.X + rx, this.Y + ry);
+        }
+    }
+}
